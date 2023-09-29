0935f4e717ccbebac4ed9882cf600bed2f921708
Son Nguyen <48971652+itms-sonnguyen@users.noreply.github.com>
2023-09-29 23:36:05 +0700

    Son Nguyen - version 29.09.2023v3
    Son Nguyen - version 29.09.2023v3

--- acs_hms/models/appointment.py
+++ acs_hms/models/appointment.py
@@ -682,11 +682,12 @@ class Appointment(models.Model):
             if template_appointment_creation:
                 for consent_id in self.consent_ids:
                     template_consent = self.env.ref('acs_hms.appointment_consent_form_email')
-                    email_values = {'sign_url': self.get_portal_url()}
-                    template_consent.with_context(**email_values).sudo().send_mail(consent_id.id, raise_exception=False)
+                    email_values = {'sign_url': consent_id.get_portal_url(), 'consent_id': consent_id}
+                    template_consent.with_context(**email_values).sudo().send_mail(self.id, raise_exception=False)
+
                 self.waiting_date_start = datetime.now()
                 self.waiting_duration = 0.1
-                self.state = 'confirm_consent'
+                self.state = 'confirm'
 
     def appointment_waiting(self):
         self.state = 'waiting'
