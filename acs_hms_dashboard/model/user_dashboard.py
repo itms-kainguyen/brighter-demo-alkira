820257083a40a9a9ebc03ca97b3fe5da17c0b0e7
singuyenitms <si.nguyen@itmsgroup.com.au>
2023-10-03 08:12:15 +0700

    update dashboard

--- acs_hms_dashboard/model/user_dashboard.py
+++ acs_hms_dashboard/model/user_dashboard.py
@@ -144,6 +144,14 @@ class ResUsers(models.Model):
                                                           ('physician_id.user_id', '=', self.env.uid)]
         self.my_total_running_treatments = Treatment.search_count(my_running_treatment_domain)
 
+        # open_prescriber
+        # physician
+        prescriber_domain = self.get_filter('date')
+        Prescriber = self.env['hms.physician']
+        self.total_prescriber = Prescriber.search_count(prescriber_domain)
+
+
+
         # Open Invoices
         Invoice = self.env['account.move'].sudo()
         open_invoice_domain = self.get_filter('invoice_date')
@@ -195,6 +203,10 @@ class ResUsers(models.Model):
     total_running_treatments = fields.Integer(compute="_compute_dashboard_data")
     total_treatments = fields.Integer(compute="_compute_dashboard_data")
     total_treatments_color = fields.Char(string='Total Treatments Color', default="#5cb85c")
+
+    # total prescriber
+    total_prescriber = fields.Integer(compute="_compute_dashboard_data")
+
 
     total_appointments = fields.Integer(compute="_compute_dashboard_data")
     total_appointments_color = fields.Char(string='Total Appointments Color', default="#f0ad4e")
@@ -411,6 +423,15 @@ class ResUsers(models.Model):
         action = self.env["ir.actions.actions"]._for_xml_id("acs_hms_base.action_physician")
         action['domain'] = self.get_filter('create_date')
         return action
+    
+    def open_online_education(self):
+
+            action = {
+                "url": '#menu_id=350&amp;action=474',
+                "type": "ir.actions.act_url",
+                'target': 'self',
+            }
+            return action
 
     def open_appointments(self):
         action = self.env["ir.actions.actions"]._for_xml_id("acs_hms.action_appointment")
@@ -422,6 +443,15 @@ class ResUsers(models.Model):
         action = self.env["ir.actions.actions"]._for_xml_id("acs_hms.acs_action_form_hospital_treatment")
         action['domain'] = self.get_filter('date')
         action['context'] = {}
+        return action
+    
+    def open_prescriber(self):
+
+        action = {
+            "url": '#menu_id=550&action=753',
+            "type": "ir.actions.act_url",
+            'target': 'self',
+        }
         return action
 
     def open_my_appointments(self):
