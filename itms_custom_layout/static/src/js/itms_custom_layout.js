548b4c2e7b2f3ac9c46d7078b6fff10e0f2b239b
singuyenitms <si.nguyen@itmsgroup.com.au>
2023-10-13 08:49:50 +0700

    video call telehealth

--- itms_custom_layout/static/src/js/itms_custom_layout.js
+++ itms_custom_layout/static/src/js/itms_custom_layout.js
@@ -3,12 +3,19 @@ function call_telehealth() {
 
     setTimeout(() => {
 
-
-        document.querySelector("div:nth-child(3) > div.o_NotificationListItem_content.o_ChannelPreviewView_content> div.o_NotificationListItem_header.o_ChannelPreviewView_header > span.o_NotificationListItem_name.o_ChannelPreviewView_name").click();
-
         setTimeout(() => {
-            document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList").click();
+            document.querySelector("div.MessagingMenuContainer > div > div > div.o_MessagingMenu_dropdownMenuHeader > button:nth-child(3)").click(); 
+            setTimeout(() => {
+                document.querySelector("div:nth-child(3) > div.o_NotificationListItem_content.o_ChannelPreviewView_content> div.o_NotificationListItem_header.o_ChannelPreviewView_header > span.o_NotificationListItem_name.o_ChannelPreviewView_name").click();
+
+                setTimeout(() => {
+                    document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList").click();
+                }, 300); 
+            }, 300);
+
+
         }, 300);
+
 
 
 
