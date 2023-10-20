bc003c442a48b226d174ae3a547cf9438fbe9ad8
singuyenitms <si.nguyen@itmsgroup.com.au>
2023-10-20 15:43:22 +0700

    fix mobile

--- itms_custom_layout/static/src/js/itms_custom_layout.js
+++ itms_custom_layout/static/src/js/itms_custom_layout.js
@@ -27,26 +27,36 @@ function call_telehealth_dashboard() {
 
 
     // click on the top bar message
-    document.querySelector("body > header > nav > div.o_menu_systray.d-flex.flex-shrink-0.ms-auto > div.MessagingMenuContainer > div > a").click();
+    if (!Object.is(document.querySelector("body > header > nav > div.o_menu_systray.d-flex.flex-shrink-0.ms-auto > div.MessagingMenuContainer > div > a"), null)) {
+        document.querySelector("body > header > nav > div.o_menu_systray.d-flex.flex-shrink-0.ms-auto > div.MessagingMenuContainer > div > a").click();
+    }
 
 
     setTimeout(() => {
 
 
         // click on the tab channels
-        document.querySelector("div.MessagingMenuContainer > div > div > div.o_MessagingMenu_dropdownMenuHeader > button:nth-child(3)").click();
+        if (!Object.is(document.querySelector("div.MessagingMenuContainer > div > div > div.o_MessagingMenu_dropdownMenuHeader > button:nth-child(3)"), null)) {
+            document.querySelector("div.MessagingMenuContainer > div > div > div.o_MessagingMenu_dropdownMenuHeader > button:nth-child(3)").click();
+        }
 
         // make smooth
 
 
         setTimeout(() => {
             // click on the prescriber button
-            document.querySelector("div:nth-child(3) > div.o_NotificationListItem_content.o_ChannelPreviewView_content> div.o_NotificationListItem_header.o_ChannelPreviewView_header > span.o_NotificationListItem_name.o_ChannelPreviewView_name").click();
+
+            if (!Object.is(document.querySelector("div:nth-child(3) > div.o_NotificationListItem_content.o_ChannelPreviewView_content> div.o_NotificationListItem_header.o_ChannelPreviewView_header > span.o_NotificationListItem_name.o_ChannelPreviewView_name"), null)) {
+                document.querySelector("div:nth-child(3) > div.o_NotificationListItem_content.o_ChannelPreviewView_content> div.o_NotificationListItem_header.o_ChannelPreviewView_header > span.o_NotificationListItem_name.o_ChannelPreviewView_name").click();
+            }
 
 
             setTimeout(() => {
                 setVisible('#loading', false);
-                document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList").click();
+                if (!Object.is(document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList"), null)) {
+                    document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList").click();
+
+                }
 
             }, 300);
         }, 300);
@@ -58,7 +68,11 @@ function call_telehealth_dashboard() {
 
 function close_telehealth() {
     setTimeout(() => {
-        document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandClose").click();
+                if (!Object.is(document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandClose"), null)) {
+
+                    document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandClose").click();
+                }
+
 
         setVisible('#loading', false);
 
@@ -76,6 +90,7 @@ function clone_telehealth_dashboard() {
 
 
 function call_close_chatbox() {
+
     $('.o_ChatWindowHeader_command.o_ChatWindowHeader_commandClose').on('click', function () {
         var style = document.createElement('style');
         style.innerHTML = `
