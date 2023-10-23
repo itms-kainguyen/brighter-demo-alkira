8ea4dbdc653e92ab4d8b8a2165764993bed6d59f
singuyenitms <si.nguyen@itmsgroup.com.au>
2023-10-23 10:46:24 +0700

    update dashboard online prescriber mobile version

--- itms_custom_layout/static/src/js/itms_custom_layout.js
+++ itms_custom_layout/static/src/js/itms_custom_layout.js
@@ -53,8 +53,8 @@ function call_telehealth_dashboard() {
 
             setTimeout(() => {
                 setVisible('#loading', false);
-                if (!Object.is(document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList"), null)) {
-                    document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList").click();
+                if (!Object.is(document.querySelector("div.o_ChatWindowHeader.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList"), null)) {
+                    document.querySelector("div.o_ChatWindowHeader.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList").click();
 
                 }
 
@@ -68,10 +68,10 @@ function call_telehealth_dashboard() {
 
 function close_telehealth() {
     setTimeout(() => {
-                if (!Object.is(document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandClose"), null)) {
+        if (!Object.is(document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandClose"), null)) {
 
-                    document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandClose").click();
-                }
+            document.querySelector("div.o_ChatWindowHeader.d-flex.align-items-center.cursor-pointer.o_ChatWindow_header > div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea> div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandClose").click();
+        }
 
 
         setVisible('#loading', false);
@@ -113,4 +113,66 @@ function setVisible(selector, visible) {
         $(selector).css("display", "none");
     }
 }
-  
+
+
+/** mobile version
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+function call_telehealth_dashboard_mobile() {
+    setVisible('#loading', true);
+
+
+
+    setTimeout(() => {
+        // click on the top bar message
+        if (!Object.is(document.querySelector("body > header > nav > div.o_menu_systray.d-flex.flex-shrink-0.ms-auto > div.MessagingMenuContainer > div > a"), null)) {
+            document.querySelector("body > header > nav > div.o_menu_systray.d-flex.flex-shrink-0.ms-auto > div.MessagingMenuContainer > div > a").click();
+
+        }
+
+        setTimeout(() => {
+
+
+            // click on the tab channels
+            if (!Object.is(document.querySelector("div.o_menu_systray> div.MessagingMenuContainer > div > div > div.o_MobileMessagingNavbar.o_MessagingMenu_mobileNavbar > div.o_MobileMessagingNavbar_tab:nth-child(3)")
+            )) {
+                document.querySelector("div.o_menu_systray> div.MessagingMenuContainer > div > div > div.o_MobileMessagingNavbar.o_MessagingMenu_mobileNavbar > div.o_MobileMessagingNavbar_tab:nth-child(3)").click();
+
+
+            }
+
+            // make smooth
+
+
+            setTimeout(() => {
+                // click on the prescriber button
+
+                if (!Object.is(document.querySelector("div:nth-child(3) > div.o_NotificationListItem_content.o_ChannelPreviewView_content> div.o_NotificationListItem_header.o_ChannelPreviewView_header > span.o_NotificationListItem_name.o_ChannelPreviewView_name"), null)) {
+
+                    document.querySelector("div:nth-child(3) > div.o_NotificationListItem_content.o_ChannelPreviewView_content> div.o_NotificationListItem_header.o_ChannelPreviewView_header > span.o_NotificationListItem_name.o_ChannelPreviewView_name").click();
+                }
+
+
+                setTimeout(() => {
+                    setVisible('#loading', false);
+                    if (!Object.is(document.querySelector("div.o_ChatWindowManager > div > div.o_ChatWindowHeader.o_ChatWindow_header> div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea > div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList"))) {
+                        document.querySelector("div.o_ChatWindowManager > div > div.o_ChatWindowHeader.o_ChatWindow_header> div.o_ChatWindowHeader_item.o_ChatWindowHeader_rightArea > div.o_ChatWindowHeader_command.o_ChatWindowHeader_commandShowMemberList").click();
+                    }
+
+                }, 300);
+            }, 300);
+
+
+        }, 300);
+
+    }, 300);
+
+
+}
