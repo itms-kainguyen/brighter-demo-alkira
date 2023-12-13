f7658cdcfe761bbc82ce81f1d8a3bbdb946af9eb
itms-nguyennguyen <112356280+itms-nguyennguyen@users.noreply.github.com>
2023-12-13 07:48:02 +0700

    update

--- app_chatgpt/models/ai_robot.py
+++ app_chatgpt/models/ai_robot.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import openai.openai_object
+# import openai.openai_object
 import requests, json
 import openai
 import base64
-
 from odoo import api, fields, models, modules, tools, _
 from odoo.exceptions import UserError
 
@@ -173,7 +172,8 @@ GPT-3	A set of models that can understand and generate natural language
     
     def get_ai_post(self, res, author_id=False, answer_id=False, param={}):
         # hook，高级版要替代
-        if res and author_id and isinstance(res, openai.openai_object.OpenAIObject) or isinstance(res, list) or isinstance(res, dict):
+        # isinstance(res, openai.openai_object.OpenAIObject) or
+        if res and author_id and isinstance(res, list) or isinstance(res, dict):
             # 返回是个对象，那么就是ai
             # if isinstance(res, dict):
             if self.provider == 'openai':
