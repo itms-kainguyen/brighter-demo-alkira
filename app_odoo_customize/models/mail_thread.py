82a4342113343faba65d3d7cfaeccd93b672cc3d
Harry Nguyen <harry.nguyen@itmsgroup.com.au>
2023-12-22 12:27:25 +0700

    ref

--- app_odoo_customize/models/mail_thread.py
+++ app_odoo_customize/models/mail_thread.py
@@ -2,7 +2,9 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models, _
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
@@ -14,7 +16,7 @@ class MailThread(models.AbstractModel):
         if app_stop_subscribe:
             return True
         else:
-            print(partner_ids)
+            _logger.info("MailThread message_subscribe partner_ids=%s"% partner_ids)
             return super(MailThread, self).message_subscribe(partner_ids, subtype_ids)
 
     def _message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None, customer_ids=None):
