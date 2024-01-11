185da27cbefca360fd7a5e961b61da3d9856b4d1
itms-nguyennguyen <112356280+itms-nguyennguyen@users.noreply.github.com>
2024-01-11 10:08:14 +0700

    update

--- acs_hms/models/analytic_line.py
+++ acs_hms/models/analytic_line.py
@@ -8,13 +8,14 @@ class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
 
     description = fields.Text('Description')
+    name = fields.Char('Name')
     location = fields.Selection(
         [('online', 'Online'), ('hospital', 'Hospital'), ('seminar', 'Seminar'), ('clinic', 'Clinic')],
         string='Location', default='online')
 
-    attachment_certification_ids = fields.Many2many('ir.attachment', 'certification_attachment_rel', 'attachment_id',
-                                                    'analytic_id', string='Certifications')
-    certification_expiry = fields.Date('Date Expiry')
+    # attachment_certification_ids = fields.Many2many('ir.attachment', 'certification_attachment_rel', 'attachment_id',
+    #                                                 'analytic_id', string='Certifications')
+    # certification_expiry = fields.Date('Date Expiry')
 
     @api.onchange('description')
     def _onchange_description(self):
