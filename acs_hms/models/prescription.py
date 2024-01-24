9bb2112072d2dd5c3b151f7495778f6d84ee80e9
itms-nguyennguyen <112356280+itms-nguyennguyen@users.noreply.github.com>
2024-01-24 10:21:50 +0700

    update

--- acs_hms/models/prescription.py
+++ acs_hms/models/prescription.py
@@ -92,6 +92,7 @@ class ACSPrescriptionOrder(models.Model):
     alert_count = fields.Integer(compute='_get_alert_count', default=0)
     old_prescription_id = fields.Many2one('prescription.order', 'Old Prescription', copy=False, states=READONLY_STATES)
     acs_kit_id = fields.Many2one('acs.product.kit', string='Template', states=READONLY_STATES)
+    medicament_group_id = fields.Many2one('medicament.group', string='Template', states=READONLY_STATES)
     acs_kit_qty = fields.Integer("Kit Qty", states=READONLY_STATES, default=1)
     expire_date = fields.Date(
         string='Expire Date',
@@ -581,6 +582,29 @@ class ACSPrescriptionOrder(models.Model):
                     'form_id': line.product_id.form_id.id,
                     'qty_per_day': line.product_id.common_dosage_id and line.product_id.common_dosage_id.qty_per_day or 1,
                     'days': line.product_id.common_dosage_id and line.product_id.common_dosage_id.days or 1,
+                    'appointment_id': appointment_id,
+                    'name': appointment_id,
+                }))
+        self.prescription_line_ids = lines
+
+    @api.onchange('medicament_group_id')
+    def get_acs_medicament_group(self):
+        # self.notes = self.acs_kit_id.description
+        lines = []
+        appointment_id = self.appointment_id and self.appointment_id.id or False
+        self.prescription_line_ids = False
+        if self.medicament_group_id:
+            for line in self.medicament_group_id.medicament_group_line_ids:
+                lines.append((0, 0, {
+                    'product_id': line.product_id.id,
+                    'common_dosage_group': line.common_dosage_id.id or False,
+                    'dose': line.dose or line.product_id.dosage,
+                    'allow_substitution': line.allow_substitution,
+                    'dosage_uom_id': line.dosage_uom_id.id if line.common_dosage_id else False,
+                    'active_component_ids': [(6, 0, [x.id for x in line.product_id.active_component_ids])],
+                    'form_id': line.product_id.form_id.id,
+                    'qty_per_day': line.qty_per_day or line.product_id.common_dosage_id and line.product_id.common_dosage_id.qty_per_day or 1,
+                    'days': line.days or line.product_id.common_dosage_id and line.product_id.common_dosage_id.days or 1,
                     'appointment_id': appointment_id,
                     'name': appointment_id,
                 }))
