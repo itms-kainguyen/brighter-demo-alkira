0352a2b5ec937082284e63d038735b377b82f618
itms-nguyennguyen <112356280+itms-nguyennguyen@users.noreply.github.com>
2024-01-24 07:26:06 +0700

    update

--- itms_consent_form/models/product.py
+++ itms_consent_form/models/product.py
@@ -8,20 +8,20 @@ class ProductProduct(models.Model):
     _inherit = 'product.product'
 
     default_aftercare_id = fields.Many2one(
-        'document.page', 
+        'bureaucrat.knowledge.document',
         string="Aftercare",
-        domain=[('type', '=', 'content'), ('parent_id.name', '=', 'After Care')])
+        domain=[('category_id.name', '=', 'After Care')])
     
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     default_aftercare_id = fields.Many2one(
-        'document.page', 
+        'bureaucrat.knowledge.document',
         string="Aftercare Template", 
         compute='_compute_default_aftercare_id', 
         inverse='_inverse_default_aftercare_id',
-        domain=[('type', '=', 'content'), ('parent_id.name', '=', 'After Care')])
+        domain=[('category_id.name', '=', 'After Care')])
 
     @api.depends('product_variant_ids', 'product_variant_ids.default_aftercare_id')
     def _compute_default_aftercare_id(self):
