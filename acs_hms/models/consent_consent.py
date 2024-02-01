e2edf84085a1c392fdcc9ef6cac4fe1c9261ec82
itms-nguyennguyen <112356280+itms-nguyennguyen@users.noreply.github.com>
2024-02-01 11:18:52 +0700

    update

--- acs_hms/models/consent_consent.py
+++ acs_hms/models/consent_consent.py
@@ -16,7 +16,7 @@ class Consent(models.Model):
             "res_id": self.id,
             "view_mode": "form",
             "view_type": "form",
-            'context': {'is_invisible': True, 'create': False},
+            'context': {'is_invisible': True, 'create': False, 'edit': False},
             "views": [(False, "form")],
             "view_id": False,
             "target": "new",
