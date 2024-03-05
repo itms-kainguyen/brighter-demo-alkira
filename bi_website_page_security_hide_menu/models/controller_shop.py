27b2cb8c587f235a7de45c3abf6278a6388a2c4b
itms-nguyennguyen <112356280+itms-nguyennguyen@users.noreply.github.com>
2024-03-05 10:03:52 +0700

    update

--- bi_website_page_security_hide_menu/models/controller_shop.py
+++ bi_website_page_security_hide_menu/models/controller_shop.py
@@ -19,13 +19,6 @@ class WebsiteSaleInherit(WebsiteSale):
             loc = '/shop/category/%s' % slug(cat)
             if not qs or qs.lower() in loc:
                 yield {'loc': loc}
+
+
     # copy standard and add auth = user
-    @http.route([
-        '/shop',
-        '/shop/page/<int:page>',
-        '/shop/category/<model("product.public.category"):category>',
-        '/shop/category/<model("product.public.category"):category>/page/<int:page>',
-    ], type='http', auth="user", website=True, sitemap=sitemap_shop)
-    def shop(self, page=0, category=None, search='', min_price=0.0, max_price=0.0, ppg=False, **post):
-        res = super(WebsiteSaleInherit,self).shop(page=0, category=None, search='', min_price=0.0, max_price=0.0, ppg=False, **post)
-        return res
