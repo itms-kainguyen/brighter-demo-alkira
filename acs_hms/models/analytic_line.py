6088a988d34bc163940d1c7c6a2e30cf59f1c354
itms-nguyennguyen <112356280+itms-nguyennguyen@users.noreply.github.com>
2024-03-15 15:21:25 +0700

    update

--- acs_hms/models/analytic_line.py
+++ acs_hms/models/analytic_line.py
@@ -20,6 +20,9 @@ class AccountAnalyticLine(models.Model):
     def _get_project_id(self):
         return self.env['project.project'].search([('name', 'ilike', 'CPD')], limit=1)
 
+    def _get_task_id(self):
+        return self.env['project.task'].search([('project_id.name', 'ilike', 'CPD')], limit=1)
+
     description = fields.Text('Description')
     name = fields.Char('Name')
     location = fields.Selection(
@@ -31,6 +34,11 @@ class AccountAnalyticLine(models.Model):
     certification_expiry = fields.Date('Date Expiry')
     channel_id = fields.Many2one('slide.channel', string="Title", index=True,
                                  ondelete='cascade')
+
+    task_id = fields.Many2one(
+        'project.task', string='Task', default=_get_task_id, index='btree_not_null',
+        compute='_compute_task_id', store=True, readonly=False,
+        domain="[('company_id', '=', company_id), ('project_id.allow_timesheets', '=', True), ('project_id', '=?', project_id)]")
 
     project_id = fields.Many2one(
         'project.project', 'Project', domain=_domain_project_id, default=_get_project_id, index=True,
